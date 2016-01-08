ca811249ba2c65b2c1fe59c15612125b77923851
Lennart Bader <mail+git@lbader.de>
2016-01-08 23:35:03 +0100

    Removed config.php

---
+++ plugins/hangman/config.php
@@ -0,0 +1,10 @@
+<?php
+    define("p_hangman_table", "p_hangman");
+    define("p_hangman_id", "id");
+    define("p_hangman_chat", "chat");
+    define("p_hangman_owner", "owner");
+    define("p_hangman_word", "word");
+    define("p_hangman_letters", "letters");
+    define("p_hangman_fails", "fails");
+    define("p_hangman_paused", "paused");
+?>
